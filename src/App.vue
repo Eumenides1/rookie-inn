11fa3d4c778a61e22d51807c408ec2a11d01ffa7
ashbur <18829526908@163.com>
2023-12-06 22:19:18 +0800

    引入vue-route并构建移动端路由表

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{isMobileTerminal}}</div>
+  <router-view />
 </template>
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import router from "./router";
 import App from './App.vue'
 import './styles/index.scss'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
