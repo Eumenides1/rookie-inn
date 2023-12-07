2193536676f086d5bcaf96bde48e28fb7b447510
ashbur <18829526908@163.com>
2023-12-07 23:21:38 +0800

    新增响应拦截器，结构服务端响应数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import responsive from "tailwindcss/lib/util/responsive";
 
 const service = axios.create({
     baseURL: import.meta.env.VITE_BASE_API,
@@ -12,5 +13,17 @@ service.interceptors.request.use(
         return config
     }
 )
+
+/**
+ * 响应拦截器
+ * 服务端返回数据之后，前端响应之前
+ */
+service.interceptors.response.use(response => {
+    const { success, message, data } = response.data
+    if (success) {
+        return data
+    }
+    return Promise.reject(new Error(message))
+})
 
 export default service
