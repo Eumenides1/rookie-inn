181d72caafddd89a1b04b6f6ef1f68daefe5888a
ashbur <18829526908@163.com>
2023-12-13 22:52:15 +0800

    适配tailwind rem基准值

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import './style.css'
 import router from "./router";
 import App from './App.vue'
 import './styles/index.scss'
+import { useREM } from "@/utils/flexible";
 
+useREM()
 createApp(App).use(router).mount('#app')
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -1,5 +1,5 @@
 import { computed } from "vue";
-import { PC_DEVICE_WIDTH } from '@/constants/index'
+import { PC_DEVICE_WIDTH } from '@/constants'
 import  { useWindowSize } from "@vueuse/core";
 
 const { width } = useWindowSize()
@@ -10,3 +10,22 @@ const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
     return width.value < PC_DEVICE_WIDTH
 })
+
+/**
+ * 动态指定rem基准值，最大为40px
+ * 根据用户的屏幕宽度进行一些计算，把计算出来的值赋值给html根标签作为fontsize大小
+ */
+export const useREM = () => {
+    // 定义最大的fontSize
+    const MAX_FONT_SIZE = 40
+    // 监听html文档被解析完成的事件
+    document.addEventListener('DOMContentLoaded', () => {
+        // 拿到html标签
+        const html = document.querySelector('html')
+        // 计算font size, 根据屏幕宽度 / 10 来获取
+        let fontSize = window.innerWidth / 10
+        fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+        // 赋值给html
+        html.style.fontSize = fontSize + 'px'
+    })
+}
