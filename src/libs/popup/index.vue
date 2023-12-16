e221f2369593322c1a4289d7b3171d17bf0b40cd
ashbur <18829526908@163.com>
2023-12-16 20:46:01 +0800

    使用useVModel 优化组件间响应式数据传递

--- src/libs/popup/index.vue
+++ src/libs/popup/index.vue
@@ -4,14 +4,14 @@
       <!--蒙板-->
       <transition name="fade">
         <div
-            v-if="modelValue"
-            @click="emits('update:modelValue', false)"
+            v-if="isVisable"
+            @click="isVisable = false"
             class="w-screen h-screen bg-zinc-900/80 z-40 fixed top-0 left-0"></div>
       </transition>
       <!--body-->
       <transition name="popup-down-up">
         <div
-            v-if="modelValue"
+            v-if="isVisable"
             v-bind="$attrs" class="w-screen bg-white z-50 fixed bottom-0">
           <slot />
         </div>
@@ -21,7 +21,7 @@
 </template>
 
 <script setup>
-import { useScrollLock } from "@vueuse/core";
+import { useScrollLock, useVModel } from "@vueuse/core";
 import {watch} from "vue";
 
 const props = defineProps({
@@ -31,15 +31,18 @@ const props = defineProps({
   }
 })
 
-const emits = defineEmits([
+defineEmits([
     'update:modelValue'
 ])
+
+// useVModel 将props 变成一个响应式数据，当 isVisable 的值发生改变的时候，会自动触发emit 修改modelValue
+const isVisable = useVModel(props)
 
 // 锁定滚动
 const isLocked = useScrollLock(document.body)
 
 watch(
-    () => props.modelValue,
+    isVisable,
     (val) => {
       isLocked.value = val
     },{
